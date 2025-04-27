5476d151559b9918e3b2e08e427d3c93eff5cbf8
jarrettg388 <jarrettclay57@gmail.com>
2025-04-27 14:03:19 -0400

    created saved parks folder

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,8 @@ export default async function Home() {
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      {data?.map((user, i) => <div key={i}>{user.name}</div>)}
+      <h1>Hello world</h1>
+      
     </div>
   );
 }
