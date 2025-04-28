182e6101d6632294f8ff23e0f578d1028751f795
DLSIII <dlstambaugh1234@gmail.com>
2025-04-28 10:27:41 -0400

    More Error fixing

---
+++ app/page1.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import MapScreen from "src/components/MapScreen";
+
+export default function ExplorePage() {
+  return (
+    <div style={{ height: "100vh", width: "100vw", overflow: "hidden" }}>
+      <MapScreen />
+    </div>
+  );
+}
